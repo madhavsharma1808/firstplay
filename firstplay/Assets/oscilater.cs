f0aab332c1671916bce4391592f1158cc01eddb7
madhavsharma1808 <madhav.189302106@muj.manipal.edu>
2020-03-18 21:11:37 +0530

    sin

---
+++ firstplay/Assets/oscilater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oscilater : MonoBehaviour
+{
+    [Range(0, 1)] [SerializeField] float position;
+    [SerializeField] Vector3 location;
+    Vector3 startingposition;
+    float tempfactor;
+    [SerializeField] float period = 2f;
+    float cycles;
+
+
+    private void Start()
+    {
+        startingposition = transform.position;
+    }
+
+    private void Update()
+    {
+        cycles = Time.time / period;
+        const float tau= Mathf.PI * 2;
+        tempfactor = Mathf.Sin(tau * cycles);
+        position = (tempfactor / 2f) + 0.5f;
+        transform.position =startingposition + location * position;
+    }
+
+   
+ 
+}
--- firstplay/Assets/scripts/die.cs
+++ firstplay/Assets/scripts/die.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class die : MonoBehaviour
 {
+    Rigidbody rigidbody;
+    [SerializeField] GameObject gameobject;
+
+    private void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("hello");
+       if(collision.gameObject.CompareTag("die"))
+        {
+            //SceneManager.LoadScene("first");
+        }
+
+       if(collision.gameObject.CompareTag("destination"))
+        {
+            //Invoke("loadscene",1f);
+            rigidbody.isKinematic = false;
+            Debug.Log("reached");
+        }
     }
+    void loadscene()
+    {
+        SceneManager.LoadScene("second");
+    }
+
 }
